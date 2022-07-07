6d2d80003d11808adc8375f2325dddb8c46a0304
Manideep Alimi <manideep.a@comakeit.com>
2022-07-07 11:22:55 +0530

    Added project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { DropdownComponent } from './dropdown/dropdown.component';
 const routes: Routes = [];
 
 @NgModule({
